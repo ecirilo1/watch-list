873a859bc88a16f5f035b69cd5c9936b9760c7df
Estelle <estelle.cirilo@gmail.com>
2021-08-12 15:45:45 +0200

    routes action view

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,15 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+end
